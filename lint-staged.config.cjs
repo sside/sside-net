6d225f00a0c17ac338a834d292475fa033d97958
sside <sside@live.jp>
2024-12-30 01:03:22 +0900

    ci: pre-commitにeslint追加

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,16 +1,5 @@
-// const { dirname, resolve } = require("path");
-// const { existsSync } = require("fs");
-
 module.exports = {
-    // "*.{ts,tsx,js,jsx}": (stagedFilePaths) =>
-    //     stagedFilePaths
-    //         .filter(
-    //             (stagedFilePath) =>
-    //                 !existsSync(
-    //                     resolve(dirname(stagedFilePath), "package.json"),
-    //                 ),
-    //         )
-    //         .map((stagedFilePath) => `eslint --fix '${stagedFilePath}'`),
+    "*.{ts,tsx,js,jsx,cjs,mjs}": ["eslint --fix --ignore-pattern *.*"],
     "*.{ts,tsx,js,jsx,mjs,cjs,json,yml,yaml,md}": ["prettier --write"],
     "schema.prisma": ["npx prisma format --schema"],
 };
