abea961855320dc3e6182f3e6a8ace8106af5cb9
sside <sside@live.jp>
2024-12-29 18:50:11 +0900

    style: import order設定

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,3 +1,12 @@
 module.exports = {
     singleAttributePerLine: true,
+    plugins: ["@trivago/prettier-plugin-sort-imports"],
+    importOrder: [
+        "^react",
+        "^next",
+        "<THIRD_PARTY_MODULES>",
+        "^@sside-net/(.*)$",
+        "^[./]",
+    ],
+    importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
 };
