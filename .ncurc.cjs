440d57b6d6be07304ea626955983a31dd3ff75d7
sside <sside@live.jp>
2025-10-15 16:00:54 +0900

    chore: アップデートしないパッケージの理由を記載

--- .ncurc.cjs
+++ .ncurc.cjs
@@ -1,3 +1,11 @@
 module.exports = {
-    reject: ["eslint", "@types/node"],
+    cooldown: 14,
+    reject: [
+        // flat configの諸々が落ち着くまで上げない
+        "eslint",
+        // プロジェクトのnodeのメジャーバージョンに合わせる
+        "@types/node",
+        // faker@10がESM onlyのため一旦バージョンを固定
+        "@faker-js/faker",
+    ],
 };
