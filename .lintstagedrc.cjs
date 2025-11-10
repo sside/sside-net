71714acd6c7df33d75559e5a938cfb1a73d53b5e
sside <sside@live.jp>
2025-11-10 21:24:05 +0900

    style: eslint設定の参照方法を変更

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
     "./(app|config|library|resource)/**/*.{ts,tsx,js,jsx,cjs}": [
-        "eslint --fix",
+        "eslint --fix --flag v10_config_lookup_from_file",
     ],
     "*.{ts,tsx,js,jsx,mjs,cjs,json,yml,yaml,md}": ["prettier --write"],
     "schema.prisma": ["npx prisma format --schema"],
