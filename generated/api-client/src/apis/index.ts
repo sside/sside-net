0308ac4491b716856367334852f7cfb437da2210
sside <sside@live.jp>
2025-11-15 17:39:36 +0900

    feat(open-api): OpenAPIベースのクライアントをopenapi-typescriptへ変更

--- generated/api-client/src/apis/index.ts
+++
@@ -1,4 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from "./AppApi";
-export * from "./BlogEntryApi";
--- generated/api-client/src/models/index.ts
+++
@@ -1,4 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from "./BlogEntryMetaTagResponse";
-export * from "./BlogEntryResponse";
