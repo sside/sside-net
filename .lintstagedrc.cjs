dd88378ec991aefeb01b2f9e5a98ca7f9d40a13a
sside <sside@live.jp>
2025-11-24 14:16:39 +0900

    ci: pre-commitをlint-stagedに集約

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,6 +1,9 @@
 module.exports = {
-    "./(app|config|library|resource)/**/*.{ts,tsx,js,jsx,cjs}": [
-        "eslint --fix --flag v10_config_lookup_from_file",
+    "./(app|config|library|resource)/**/*.{ts,tsx,js,jsx,cjs}": (
+        stagedFiles,
+    ) => [
+        `eslint --fix --flag v10_config_lookup_from_file ${stagedFiles.join(" ")}`,
+        "npm run test",
     ],
     "*.{ts,tsx,js,jsx,mjs,cjs,json,yml,yaml,md}": ["prettier --write"],
     "schema.prisma": ["npx prisma format --schema"],
