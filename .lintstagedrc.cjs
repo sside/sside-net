6666529a8fbe7ce5c96d13af7b2a7e6c44cdfece
sside <sside@live.jp>
2026-03-25 14:41:10 +0900

    chore: update deps

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,10 +1,7 @@
 module.exports = {
     "./(app|config|library|resource)/**/*.{ts,tsx,js,jsx,cjs}": (
         stagedFiles,
-    ) => [
-        `eslint --fix --flag v10_config_lookup_from_file ${stagedFiles.join(" ")}`,
-        "npm run test",
-    ],
+    ) => [`eslint --fix ${stagedFiles.join(" ")}`, "npm run test"],
     "*.{ts,tsx,js,jsx,mjs,cjs,json,yml,yaml,md}": ["prettier --write"],
     "schema.prisma": ["npx --no-install prisma format --schema"],
 };
