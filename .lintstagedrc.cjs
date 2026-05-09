2e76f34d1bfcb34c58a7f3fc13ea79e1d99e5fff
sside <sside@live.jp>
2026-05-09 17:53:01 +0900

    ci: packageごとにテスト実行を分けた

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,7 +1,17 @@
 module.exports = {
     "./(app|config|library|resource)/**/*.{ts,tsx,js,jsx,cjs}": (
         stagedFiles,
-    ) => [`eslint --fix ${stagedFiles.join(" ")}`, "npm run test"],
+    ) => [`eslint --fix ${stagedFiles.join(" ")}`],
+    // フロントエンドのテストはバックエンド不要になるまで一旦見送り
+    // "./app/frontend/**/*.{ts,tsx,js,jsx,cjs}": [
+    //     "npx --no-install turbo run test --filter=@sside-net/frontend",
+    // ],
+    "./app/backend/**/*.{ts,tsx,js,jsx,cjs}": [
+        "npx --no-install turbo run test --filter=@sside-net/backend",
+    ],
+    "./(config|library|resource)/**/*.{ts,tsx,js,jsx,cjs}": [
+        "npx --no-install turbo run test --filter=!./app/*",
+    ],
     "*.{ts,tsx,js,jsx,mjs,cjs,json,yml,yaml,md}": ["prettier --write"],
     "schema.prisma": ["npx --no-install prisma format --schema"],
 };
