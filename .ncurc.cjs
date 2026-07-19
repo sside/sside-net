2de1e174bb10ab5287a7a3e968dcbed6d0c494cc
sside <sside@live.jp>
2026-07-19 21:32:22 +0900

    chore: update deps

--- .ncurc.cjs
+++ .ncurc.cjs
@@ -1,8 +1,11 @@
 module.exports = {
+    cooldown: 14,
     reject: [
         // プロジェクトのnodeのメジャーバージョンに合わせる
         "@types/node",
         // faker@10がESM onlyのため一旦バージョンを固定
         "@faker-js/faker",
+        // TypeScript 7.1まで見送り
+        "typescript",
     ],
 };
