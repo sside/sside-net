d8adcefc1ae2f97ec5087b20ba74c11a8570931c
sside <sside@live.jp>
2026-08-02 18:27:03 +0900

    feat(frontend): not-foundページ追加

---
+++ app/frontend/app/blog/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <div className="not-found grid gap-2 pt-8">
+            <header className="text-4xl font-bold">Page not found.</header>
+            <span className="text-xl">See the contents in the side menu.</span>
+        </div>
+    );
+}
