c98cc352aa9a1ca9095b0ce068da3bd55b0cce80
sside <sside@live.jp>
2026-08-18 17:13:58 +0900

    test(frontend): カラ実装になっていたのでテスト追加

--- app/frontend/app/sign-in/SignInForm.test.ts
+++ app/frontend/app/sign-in/SignInForm.test.ts
@@ -1,11 +1,26 @@
 import { expect, test } from "next/experimental/testmode/playwright/msw";
+import { mockManagementRoot } from "../management/_test/mockManagementRoot";
 
 test.describe("SignInForm", () => {
-    test.beforeEach(async ({ page }) => {
+    test.beforeEach(async ({ page, msw }) => {
+        mockManagementRoot(msw);
+
         await page.goto("/sign-in");
     });
 
-    test("", async ({ page }) => {
-        await expect(page.locator(".sign-in-form"));
+    test("入力がない場合、ログインボタンが押せないこと。", async ({ page }) => {
+        const locator = page.locator(".sign-in-form");
+        const passwordInput = locator.getByRole("textbox", {
+            name: "password",
+        });
+        await expect(passwordInput).toBeVisible();
+
+        const submitButton = locator.getByRole("button", {
+            name: "sign in",
+        });
+        await expect(submitButton).toBeDisabled();
+
+        await passwordInput.fill(process.env.ADMINISTRATOR_PASSWORD!);
+        await expect(submitButton).not.toBeDisabled();
     });
 });
