76edeb0701126b312334d32e934fc9651419e198
sside <sside@live.jp>
2026-08-30 02:05:35 +0900

    refactor(frontend): ヘッダ部分を一つのコンポーネントにまとめた

--- app/frontend/app/blog/_menu/BlogMenuSection.tsx
+++ app/frontend/app/blog/_menu/BlogMenuSection.tsx
@@ -1,17 +1,28 @@
 import { FC } from "react";
+import { twMerge } from "tailwind-merge";
 import { VerticalDivider } from "../../../component/divider/VerticalDivider";
 import { ChildrenProp } from "../../../type/ChildrenProp";
-import { BlogMenuSectionHeader } from "./BlogMenuSectionHeader";
 
 export const BlogMenuSection: FC<{ headerLabel: string } & ChildrenProp> = ({
     headerLabel,
     children,
 }) => {
+    const horizontalPadding = "px-2";
+
     return (
         <div className="grid w-full">
-            <BlogMenuSectionHeader>{headerLabel}</BlogMenuSectionHeader>
-            <VerticalDivider className="w-3/4" />
-            {children}
+            <header
+                className={twMerge(
+                    "w-full text-xl font-light",
+                    horizontalPadding,
+                )}
+            >
+                {headerLabel}
+            </header>
+            <VerticalDivider />
+            <div className={twMerge("py-1 font-light", horizontalPadding)}>
+                {children}
+            </div>
         </div>
     );
 };
--- app/frontend/app/blog/_menu/BlogMenuSectionHeader.tsx
+++
@@ -1,6 +0,0 @@
-import { FC } from "react";
-import { ChildrenProp } from "../../../type/ChildrenProp";
-
-export const BlogMenuSectionHeader: FC<{} & ChildrenProp> = ({ children }) => {
-    return <header className="w-full text-xl">{children}</header>;
-};
